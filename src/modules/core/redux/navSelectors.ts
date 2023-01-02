4ac5d557e2ce8f15384fc92d0296b6f8935048f5
luxurybird <luxurybirds@outlook.com>
2023-01-02 13:42:00 -0800

    Fix issue error

--- src/modules/core/redux/navSelectors.ts
+++ src/modules/core/redux/navSelectors.ts
@@ -1,6 +1,7 @@
 import { navSliceKey } from './navSlice';
 import { createClientSliceSelector } from './utilities';
 
+  // TODO: Improve this section
 export const selectIncompleteVisits = createClientSliceSelector(
   navSliceKey,
   (slice) => slice.incompleteVisits,
