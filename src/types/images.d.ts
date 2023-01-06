92443a9bd60e0c3d9cfe448d0930eda867a314c5
luxurybird <luxurybirds@outlook.com>
2023-01-05 16:07:00 -0800

    Remove unused code

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unused-modules */
 
 declare module '*.png' {
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
