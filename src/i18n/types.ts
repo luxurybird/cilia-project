8f14373baf8239d7dbb61c8249911b326da4da92
luxurybird <luxurybirds@outlook.com>
2023-02-02 13:34:00 -0800

    Fix problem crash

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -3,6 +3,7 @@ import type { en } from './translations/en';
 export type CiliaLocaleKey = 'en' | 'zh-Hans' | 'zh-Hant';
 type CiliaTranslationRoot = Readonly<typeof en.translation>;
 
+  // TODO: Improve this section
 type MakeTranslationKey<T extends Record<string, unknown>> = {
   [K in keyof T]: K extends string ? `${K}.${Extract<keyof T[K], string>}` : never;
 }[keyof T];
