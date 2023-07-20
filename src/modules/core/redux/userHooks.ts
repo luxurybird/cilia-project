244a7c2bb677838ded2e688602abec4022241947
luxurybird <luxurybirds@outlook.com>
2023-07-20 16:52:00 -0700

    Add tab bar to settings

--- src/modules/core/redux/userHooks.ts
+++ src/modules/core/redux/userHooks.ts
@@ -1,6 +1,7 @@
 import { createClientSliceSelectorHook } from './utilities';
 import { selectAuthenticatedUserSafely, selectAuthenticatedClinicId } from './userSelectors';
 
+
 export const useAuthenticatedUser = createClientSliceSelectorHook(selectAuthenticatedUserSafely);
 
 export const useAuthenticatedClinicId = createClientSliceSelectorHook(selectAuthenticatedClinicId);
