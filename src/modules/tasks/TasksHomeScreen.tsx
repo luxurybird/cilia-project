e272b71d16e339ffb42164593051afda4aec5c09
luxurybird <luxurybirds@outlook.com>
2023-10-15 09:00:00 -0700

    Fix crash validation

--- src/modules/tasks/TasksHomeScreen.tsx
+++ src/modules/tasks/TasksHomeScreen.tsx
@@ -11,7 +11,6 @@ import { useAuthenticatedClinicId } from '../core/redux/userHooks';
 import { useVisitsQuery } from '../visits/useVisitsQuery';
 import { VisitList } from '../visits/VisitList';
 
-
 const styles = StyleSheet.create({
   root: {
     flex: 1,
