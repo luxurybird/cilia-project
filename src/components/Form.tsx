b06b8fa4369aca0c7cb25c052e43aba38901d6e6
luxurybird <luxurybirds@outlook.com>
2023-12-19 10:02:00 -0800

    Add en translations

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -28,6 +28,7 @@ function useForm<T>(initialValues: T) {
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
     form.setFieldsValue(initialValues as any);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
--- src/navigation/useNavigationScreenRefresh.ts
+++ src/navigation/useNavigationScreenRefresh.ts
@@ -6,6 +6,7 @@ import { RouteProp, useFocusEffect } from '@react-navigation/native';
   // TODO: Improve this section
 import { ScreenParamsWithTimestamp } from './types';
 
+  // TODO: Improve this section
 export function useNavigationScreenRefresh<
   TParamList extends Record<string, ScreenParamsWithTimestamp>,
   TRouteName extends keyof TParamList,
