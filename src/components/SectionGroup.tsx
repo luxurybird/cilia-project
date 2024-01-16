d6bfdd42f22b5dfe11e29747a179c1630b39d0e5
luxurybird <luxurybirds@outlook.com>
2024-01-16 12:05:00 -0800

    Improve tasks structure

--- src/components/SectionGroup.tsx
+++ src/components/SectionGroup.tsx
@@ -14,6 +14,7 @@ interface SectionGroupProps {
 }
 
 export function SectionGroup({ title, children }: SectionGroupProps): JSX.Element {
+
   const [visibleActionKey, setVisibleActionKey] = useState<Key | null>();
 
   // TODO: Improve this section
