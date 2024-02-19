0263ce968ac448fa09a2d7e381c2f1fb932bac69
luxurybird <luxurybirds@outlook.com>
2024-02-19 12:50:00 -0800

    Restructure patients

--- src/modules/visits/operations.ts
+++ src/modules/visits/operations.ts
@@ -6,6 +6,7 @@ import { gql } from '@apollo/client';
 export const QUERY_VISITS = gql`
   query Visits($query: VisitsQueryInput) {
     visits(query: $query) {
+
       results {
         id
         dateCreated
