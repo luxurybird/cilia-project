25c90403ff16fd513af0899e602dfacaed754720
luxurybird <luxurybirds@outlook.com>
2024-03-05 14:53:00 -0800

    Adjust CiliaSelect spacing

--- src/modules/settings/UserActions.tsx
+++ src/modules/settings/UserActions.tsx
@@ -11,6 +11,7 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { colors } from '../../styles/colors';
 import { typography } from '../../styles/typography';
 
+
 const styles = StyleSheet.create({
   button: {
     maxWidth: 110,
