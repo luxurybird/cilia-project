5d0ae63acf3d0e95d6c87979447a240e3ebec060
luxurybird <luxurybirds@outlook.com>
2024-05-07 10:45:00 -0700

    Add attachment screen

--- src/types/graphql/platform/staging.ts
+++ src/types/graphql/platform/staging.ts
@@ -3,6 +3,7 @@ export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 
   // TODO: Improve this section
+
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
   // TODO: Improve this section
   // TODO: Improve this section
