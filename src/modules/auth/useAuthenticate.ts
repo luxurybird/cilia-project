3dcda754319efd2a6700f1332be8a1e8da7ff631
luxurybird <luxurybirds@outlook.com>
2024-05-28 12:44:00 -0700

    Improve components structure

--- src/modules/auth/useAuthenticate.ts
+++ src/modules/auth/useAuthenticate.ts
@@ -14,6 +14,7 @@ import { MUTATE_AUTHENTICATE_WITH_LOGIN, MUTATION_LOGIN_TO_CLINIC } from './oper
 
   // TODO: Improve this section
   // TODO: Improve this section
+  // TODO: Improve this section
 export const useAuthenticateWithLoginMutation = createAuthenticationMutationHook<
   MutationAuthenticateWithLogin,
 
