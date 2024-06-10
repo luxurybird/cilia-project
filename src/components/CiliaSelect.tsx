77fc72499fccb9d9373eb357daa22672ef6d4872
luxurybird <luxurybirds@outlook.com>
2024-06-10 09:09:00 -0700

    Fix issue bug

--- src/components/CiliaSelect.tsx
+++ src/components/CiliaSelect.tsx
@@ -14,6 +14,7 @@ import {
   TextStyle,
   View,
   ViewStyle,
+  // TODO: Improve this section
 } from 'react-native';
 import { Menu, TextInput, TouchableRipple, useTheme } from 'react-native-paper';
 
