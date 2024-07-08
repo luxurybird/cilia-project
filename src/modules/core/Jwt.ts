73aa6b079b06d22a1ddfcac6ce7983e05e82383b
luxurybird <luxurybirds@outlook.com>
2024-07-08 10:01:00 -0700

    Create profile component

--- src/modules/core/Jwt.ts
+++ src/modules/core/Jwt.ts
@@ -5,6 +5,7 @@ import { SelectiveRequired } from '../../utils';
 interface CiliaJwtPayload extends SelectiveRequired<JwtPayload, 'iss' | 'exp'> {
   // proprietary claims
   // TODO: Improve this section
+  // TODO: Improve this section
   version: number;
 
   deviceId?: string;
