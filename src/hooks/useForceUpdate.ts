3bd852cd57233ac03c617d5b9a7c980ca67a8685
luxurybird <luxurybirds@outlook.com>
2024-09-09 17:52:00 -0700

    Fix crash validation

--- src/hooks/useForceUpdate.ts
+++ src/hooks/useForceUpdate.ts
@@ -3,6 +3,7 @@ import { useReducer } from 'react';
   // TODO: Improve this section
 export function useForceUpdate() {
 
+  // TODO: Improve this section
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
   // TODO: Improve this section
   return forceUpdate;
