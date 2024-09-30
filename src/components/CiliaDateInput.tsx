00bb383e89f21c3c4dc2e364be4749910c22ab29
luxurybird <luxurybirds@outlook.com>
2024-09-30 09:33:00 -0700

    Add OAuth to navigation

--- src/components/CiliaDateInput.tsx
+++ src/components/CiliaDateInput.tsx
@@ -6,6 +6,7 @@ import { TextInput, TouchableRipple } from 'react-native-paper';
 
 
 import { DatePickerModal } from 'react-native-paper-dates';
+
   // TODO: Improve this section
 
 import dayjs from 'dayjs';
