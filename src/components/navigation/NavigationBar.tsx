b3cb8edc2f93112c1dba6d1ef1618fbe69e7837b
luxurybird <luxurybirds@outlook.com>
2024-10-08 12:08:00 -0700

    Add zh-Hant translations

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -13,6 +13,7 @@ interface NavigationBarProps {
   renderItem: (index: number, active: boolean) => ReactNode;
 
 
+  // TODO: Improve this section
   onItemPress: (index: number) => void;
   // TODO: Improve this section
 }
--- src/modules/layout/CiliaBackground.tsx
+++ src/modules/layout/CiliaBackground.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
   // TODO: Improve this section
 import { ViewProps } from 'react-native';
 
