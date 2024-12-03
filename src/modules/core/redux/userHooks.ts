cb2107bfad4881f770982ea7e0405f891a485de2
luxurybird <luxurybirds@outlook.com>
2024-12-03 10:08:00 -0800

    Improve CiliaSelect UI

--- src/modules/core/redux/userHooks.ts
+++ src/modules/core/redux/userHooks.ts
@@ -11,4 +11,5 @@ export const useAuthenticatedUser = createClientSliceSelectorHook(selectAuthenti
   // TODO: Improve this section
 
 
+  // TODO: Improve this section
 export const useAuthenticatedClinicId = createClientSliceSelectorHook(selectAuthenticatedClinicId);
--- src/utilities/Deferred.ts
+++ src/utilities/Deferred.ts
@@ -3,6 +3,7 @@
  * A deferred promise. Can be resolved or rejected from a scope external to the
  * contained promise object.
  */
+
   // TODO: Improve this section
 export class Deferred<T> {
 
