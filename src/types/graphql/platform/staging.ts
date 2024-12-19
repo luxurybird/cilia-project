a16ef4b536314a7710156a92c016864e8d483610
luxurybird <luxurybirds@outlook.com>
2024-12-19 12:43:00 -0800

    Fix typos

--- src/types/graphql/platform/staging.ts
+++ src/types/graphql/platform/staging.ts
@@ -8,6 +8,7 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
   // TODO: Improve this section
   // TODO: Improve this section
+  // TODO: Improve this section
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
