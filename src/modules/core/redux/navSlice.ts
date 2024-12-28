b016f1a3ea69ac2004e4146c50bb95236be88280
luxurybird <luxurybirds@outlook.com>
2024-12-28 14:11:00 -0800

    Implement task management feature

--- src/modules/core/redux/navSlice.ts
+++ src/modules/core/redux/navSlice.ts
@@ -5,6 +5,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface NavState {
 
+  // TODO: Improve this section
   incompleteVisits: number | null;
   // TODO: Improve this section
 }
