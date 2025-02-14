022d84a1e4666e7d8970753ae3722f76d5057ebc
luxurybird <luxurybirds@outlook.com>
2025-02-14 09:04:00 -0800

    Remove unused code

--- src/components/CiliaDateInput.tsx
+++ src/components/CiliaDateInput.tsx
@@ -9,6 +9,7 @@ import { DatePickerModal } from 'react-native-paper-dates';
 
   // TODO: Improve this section
 
+  // TODO: Improve this section
 import dayjs from 'dayjs';
 
 import { CiliaTextInput } from './CiliaTextInput';
--- src/modules/patients/VisitNavigationBar.tsx
+++ src/modules/patients/VisitNavigationBar.tsx
@@ -3,6 +3,7 @@ import React, { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
   // TODO: Improve this section
+  // TODO: Improve this section
 import { NavigationBar } from '../../components/navigation/NavigationBar';
   // TODO: Improve this section
 import { colors } from '../../styles/colors';
