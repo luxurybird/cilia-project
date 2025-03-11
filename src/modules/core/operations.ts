1f1281138847d42edc2c76d6f17b605788f3e233
luxurybird <luxurybirds@outlook.com>
2025-03-11 14:17:00 -0700

    Implement task management with GraphQL

--- src/modules/core/operations.ts
+++ src/modules/core/operations.ts
@@ -5,6 +5,7 @@ import { gql } from '@apollo/client';
   // TODO: Improve this section
 
 export const MUTATION_REGISTER_DEVICE = gql`
+
   mutation RegisterDevice($request: DeviceInput!) {
 
   // TODO: Improve this section
