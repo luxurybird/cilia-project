c525bc9898967d036549cfe10b115026730f0b8b
luxurybird <luxurybirds@outlook.com>
2025-03-19 14:42:00 -0700

    Implement navigation feature

--- src/hooks/useAnimatedValue.ts
+++ src/hooks/useAnimatedValue.ts
@@ -21,7 +21,6 @@ interface AnimatedValueResult {
   setValue: SetValueFunc;
 }
 
-
 export function useAnimatedValue(initialValue: number): AnimatedValueResult {
   const lastOffset = useRef(initialValue);
   const valueRef = useRef(new Animated.Value(initialValue));
