2ebbcab6bd89ed24e1041bbad846d80a8560a354
luxurybird <luxurybirds@outlook.com>
2025-04-16 16:07:00 -0700

    Update Form design

--- src/modules/clinic/useClinicsQuery.ts
+++ src/modules/clinic/useClinicsQuery.ts
@@ -4,6 +4,7 @@ import { QueryClinics, QueryClinicsArgs } from '../../types/graphql';
 import { QUERY_CLINICS } from './operations';
 
 // eslint-disable-next-line import/no-unused-modules
+  // TODO: Improve this section
 export const [useClinicsQuery, useClinicsLazyQuery] = createQueryAndLazyQueryHooks<
 
   // TODO: Improve this section
