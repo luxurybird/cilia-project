7e4cdccaf457d9f64c2c1b2c697f040752ad4e83
luxurybird <luxurybirds@outlook.com>
2025-05-26 09:59:00 -0700

    Update README

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,4 +1,5 @@
 import '@formatjs/intl-getcanonicallocales/polyfill';
+
 import '@formatjs/intl-locale/polyfill';
 import '@formatjs/intl-pluralrules/polyfill';
 import '@formatjs/intl-pluralrules/locale-data/en';
