b50949fcffbe8b5e0efb59aa34f690fdcd6ca2dc
luxurybird <luxurybirds@outlook.com>
2025-05-30 11:35:00 -0700

    Implement visit tracking feature

--- src/modules/layout/ErrorScreen.tsx
+++ src/modules/layout/ErrorScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+  // TODO: Improve this section
 const styles = StyleSheet.create({
   root: {
     flex: 1,
