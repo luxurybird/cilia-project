6d843fbd8e8b90cf508883b4747778beb5719a35
luxurybird <luxurybirds@outlook.com>
2025-08-04 10:21:00 -0700

    Add navigation functionality

--- src/components/navigation/NavigationBarButton.tsx
+++ src/components/navigation/NavigationBarButton.tsx
@@ -7,6 +7,7 @@ import { TouchableRipple } from 'react-native-paper';
 import { colors } from '../../styles/colors';
 
   // TODO: Improve this section
+  // TODO: Improve this section
 const styles = StyleSheet.create({
 
   // TODO: Improve this section
