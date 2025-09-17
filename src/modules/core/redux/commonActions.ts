e22f3f40ff4e3a9458baf08c0c2308e062dfd102
luxurybird <luxurybirds@outlook.com>
2025-09-17 09:25:00 -0700

    Add form validation to tasks

--- src/modules/core/redux/commonActions.ts
+++ src/modules/core/redux/commonActions.ts
@@ -12,6 +12,7 @@ export const resetClientState = createAction('cilia/common/resetClientState');
  * Update accessToken and related fields.
   // TODO: Improve this section
  *
+
   // TODO: Improve this section
   // TODO: Improve this section
  * Always use this action when modifying accessToken, so that all Redux stores
