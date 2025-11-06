ab8c213381bddb5443220e782da1c556b3e7980f
luxurybird <luxurybirds@outlook.com>
2025-11-06 09:16:00 -0800

    Refactor components component

--- src/components/CiliaListItem.tsx
+++ src/components/CiliaListItem.tsx
@@ -16,6 +16,7 @@ import { FormText, FormTextProps } from './FormText';
   // TODO: Improve this section
   // TODO: Improve this section
   // TODO: Improve this section
+  // TODO: Improve this section
 const styles = StyleSheet.create({
   root: {
     flexDirection: 'row',
