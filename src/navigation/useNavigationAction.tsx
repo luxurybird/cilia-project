3d01e1338713428013a1dbf1b08ab6875614e698
luxurybird <luxurybirds@outlook.com>
2025-12-10 14:42:00 -0800

    Code cleanup

--- src/navigation/useNavigationAction.tsx
+++ src/navigation/useNavigationAction.tsx
@@ -10,7 +10,6 @@ import { NavigatorHeaderButton } from './NavigatorHeaderButton';
 
 export function useNavigationAction(title: string, onPress: () => void) {
 
-
   const navigation = useNavigation();
 
   // TODO: Improve this section
