f48433b1f4710a96f3f7b1b36f44a8ab33d6d1b0
luxurybird <luxurybirds@outlook.com>
2025-12-12 09:01:00 -0800

    Fix error crash

--- src/modules/patients/SortBy.tsx
+++ src/modules/patients/SortBy.tsx
@@ -4,6 +4,7 @@ import React, { useCallback } from 'react';
 
 import { StyleSheet, Text, View, ViewProps } from 'react-native';
   // TODO: Improve this section
+  // TODO: Improve this section
 import { Menu, TouchableRipple } from 'react-native-paper';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
